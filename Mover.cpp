3fda27e554f25d02408d8080fd18797a8a1bbf8f
Nickolas-Barandich <nick@trytec.ca>
2025-11-15 23:40:58 -0500

    Refactor calibration logic in Mover class: update max degrees, rename functions, and enhance Y-axis calibration with X-axis integration.

--- Mover.cpp
+++ Mover.cpp
@@ -80,8 +80,8 @@ bool RemoteControlCodeEnabled = true;
 // Allows for easier use of the VEX Library
 using namespace vex;
 
-const int MAX_DEGREES_X = 999999;
-const int MAX_DEGREES_Y = 999999;
+const int MAX_DEGREES_X = -1800;
+const int MAX_DEGREES_Y = -3500;
 
 class Mover {
   private:
@@ -372,19 +372,19 @@ class Mover {
     }
 };
 
-void calibrateYaxis()
+void calibrateAllAxes()
 {
   const int SAMPLES = 3;
   const double APPROACH_SPEED = 10.0; //towards sensor
   const double RETRACT_SPEED = 20; //away from sensor
 
 
-  double samples[SAMPLES];
+  double samplesY[SAMPLES];
 
-  for (int i = 0; i < SAMPLES; i++) //move away until not prressed
+  for (int i = 0; i < SAMPLES; i++) //move away until not pressed
   {
     YMotor.setVelocity(RETRACT_SPEED, percent);
-    YMotor.spin(reverse);
+    YMotor.spin(forward);
     while (YCalSwitch.pressing()) 
     {
     }
@@ -394,23 +394,51 @@ void calibrateYaxis()
     //move slowly toards sernsor unitl pressed
 
     YMotor.setVelocity(APPROACH_SPEED, percent);
-    YMotor.spin(forward);
+    YMotor.spin(reverse);
     while (!YCalSwitch.pressing()) 
     {
     }
     YMotor.stop(brake);
     //records position
-    samples[i] = YMotor.position(degrees);
-    
+    samplesY[i] = YMotor.position(degrees);
 
     //pause to settle 
-    wait(200,msec);
+    wait(2000,msec);
+
+    //calibrate x axis
   }
   //average contact position calculation
-  double avgDeg = samples[0] + samples[1] + samples[2] / SAMPLES;
-  //shift encoder so the positon becomes 0
+  double avgDeg = (samplesY[0] + samplesY[1] + samplesY[2]) / SAMPLES;
+  //shift encoder so the position becomes 0
   double currentDeg = YMotor.position(degrees);
   YMotor.setPosition(currentDeg - avgDeg, degrees);
+
+    //x axis calibration
+    double samplesX[SAMPLES];
+
+    for (int i = 0; i < SAMPLES; i++) //move away until not pressed
+    {
+      XMotor.setVelocity(RETRACT_SPEED, percent);
+      XMotor.spin(forward);
+      while (XCalDistance.objectDistance(mm) < 60) 
+      {
+      }
+
+      //mmove slowly towards sensor until 
+      XMotor.setVelocity(APPROACH_SPEED*2, percent);
+      XMotor.spin(reverse);
+      while (XCalDistance.objectDistance(mm) > 40) 
+      {
+      }
+      XMotor.stop(brake);
+      //records position
+      samplesX[i] = XMotor.position(degrees);
+    }
+
+    //average contact position calculation
+    double avgDegX = (samplesX[0] + samplesX[1] + samplesX[2]) / SAMPLES;
+    double currentDegX = XMotor.position(degrees);
+    XMotor.setPosition(currentDegX - avgDegX, degrees);
 }
 
 void manualControlOverride()
@@ -423,18 +451,18 @@ void manualControlOverride()
   while (!Controller.ButtonFDown.pressing())
   {
     //moves along the x-axis
-    if (XCalDistance.objectDistance(mm) == 0 && Controller.AxisB.position() <= 0)
+    if ((XMotor.position(degrees) >= 0 && Controller.AxisB.position() <= 0) || (XMotor.position(degrees) <= MAX_DEGREES_X/2 && Controller.AxisB.position() >= 0))
     {
       //stops moving if it reaches the boundary and tries to go further
       XMotor.setVelocity(0, percent);
     }
     else
     {
-      XMotor.setVelocity(Controller.AxisB.position(), percent);
+      XMotor.setVelocity(Controller.AxisB.position()/3, percent);
     }
 
     //moves along the y-axis
-    if (YCalSwitch.pressing() && Controller.AxisA.position() <= 0)
+    if ((YMotor.position(degrees) >= 0 && Controller.AxisA.position() <= 0) || (YMotor.position(degrees) <= MAX_DEGREES_Y/2 && Controller.AxisA.position() >= 0))
     {
       //stops moving if it reaches the boundary and tries to go further
       YMotor.setVelocity(0, percent);
@@ -450,22 +478,23 @@ void manualControlOverride()
       markerDown = true;
       MMotor.setVelocity(100, percent);
       MMotor.spin(forward);
-      wait(100, msec);
+      wait(200, msec);
       MMotor.stop(brake);
       markerInProgress = false;
     }
     //moves marker up if pressed and marker not already down
-    if (!Controller.ButtonRDown.pressing() && markerDown == true && markerInProgress == false)
+    if (Controller.ButtonRUp.pressing() && markerDown == true && markerInProgress == false)
     {
       markerInProgress = true;
       markerDown = false;
       MMotor.setVelocity(100, percent);
       MMotor.spin(reverse);
-      wait(100, msec);
+      wait(200, msec);
       MMotor.stop(brake);
       markerInProgress = false;
-
     }
+    Brain.Screen.print("X: %d Y: %d", XMotor.position(degrees), YMotor.position(degrees));
+    Brain.Screen.clearLine();
 
   }
   XMotor.stop(brake);
@@ -481,7 +510,7 @@ int main() {
 
   // Begin project code
 
-  
+  calibrateAllAxes();
   manualControlOverride();
 
   Mover m;
